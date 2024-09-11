6c3eb7c52b568335ab257a42ec7ad8790515844f
Artcraft <arturoao@uma.es>
2024-09-11 10:46:54 +0200

    Added exercise 12, unfinished

--- src/main/java/practica0/Factorial.java
+++ src/main/java/practica0/Factorial.java
@@ -3,14 +3,27 @@
 package practica0;
 
 public class Factorial {
+    private int sumTot =0;
      public int calcFactorial(int num){
          if(num<0){
              throw new IllegalArgumentException("El numero al que se le quiere calcular el factorial no puede ser negativo");
          }
          int factFinal = 0;
          for(int i = 0; i<=num; i++){
-             factFinal+=i;
+             factFinal*=i;
          }
          return factFinal;
+    }
+
+    /*Ejercicio 12 del pdf Practica 0*/
+    public int factSum(int n){
+        return sumTot*=n;
+    }
+    public int calcPorRecursividad(int n){
+        int aux = 0;
+        for(int i = 0; i<n; i++){
+            aux*=factSum(i);
+        }
+        return aux;
     }
 }
