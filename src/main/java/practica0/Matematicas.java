2e8da1d4028b7570863087662de9a1121a3ff56d
Artcraft <arturoao@uma.es>
2024-09-12 09:14:40 +0200

    Finished exercise 14, solved

--- src/main/java/practica0/Matematicas.java
+++ src/main/java/practica0/Matematicas.java
@@ -14,9 +14,31 @@ public class Matematicas {
         }
     }
 
+    /*Ejercicio 14 del pdf Practica 0*/
+    public static int ackermann(int m, int n){
+        if(m<0||n<0){
+            throw new IllegalArgumentException("Ninguno de los dos numeros puede ser negativo");
+        } else if (m > 3) {
+            throw new IllegalArgumentException("El valor de m es demasiado grande, puede causar un desbordamiento de pila.");
+        }
+        if(m==0){
+            return n+1; 
+        } else if (n==0) {
+            return ackermann(m-1, 1);
+        }else {
+            return ackermann(m-1, ackermann(m, n-1));
+        }
+    }
     public static void main(String[] args){
+        /*Testeo para ej13*/
         int ejemplo = 20;
         int resultEjemplo = fibonacci(ejemplo);
         System.out.println("La serie de Fibonacci del numero "+ejemplo+" es "+resultEjemplo);
+
+        /*Testeo ej14*/
+        int ejemploM = 3;
+        int ejemploN = 3;
+        int resultEj14=ackermann(ejemploM, ejemploN);
+        System.out.println("La serie de Ackermann para los numeros M ("+ejemploM+") y N ("+ejemploN+") da como resultado: "+resultEj14);
     }
 }
