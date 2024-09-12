f81cf4ec3a188fef8f7a9afd151fdeac7b489406
Artcraft <arturoao@uma.es>
2024-09-12 09:59:07 +0200

    Added the solution for the final exercise (16), refactored

--- src/main/java/practica0/Matematicas.java
+++ src/main/java/practica0/Matematicas.java
@@ -1,6 +1,9 @@
 /*Ejercicio 13 del pdf Practica 0*/
 package practica0;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Matematicas {
     public static int fibonacci(int n) {
         if (n < 0) {
@@ -47,22 +50,39 @@ public class Matematicas {
         return mcd(n, m % n);
     }
 
+    /*Ejercicio 16 del pdf Practica 0*/
+    public static void mostrarLista(List<Integer> lista, int i){
+        if(i>lista.size()){
+            return;
+        }
+        mostrarLista(lista, i+1);
+        System.out.println(lista.get(i-1));
+    }
     public static void main(String[] args) {
         /*Testeo para ej13*/
         int ejemplo = 20;
         int resultEjemplo = fibonacci(ejemplo);
-        System.out.println("La serie de Fibonacci del numero " + ejemplo + " es " + resultEjemplo);
+        System.out.println("La serie de Fibonacci del numero " + ejemplo + " es " + resultEjemplo+"\n");
 
         /*Testeo ej14*/
         int ejemploM = 3;
         int ejemploN = 3;
         int resultEj14 = ackermann(ejemploM, ejemploN);
-        System.out.println("La serie de Ackermann para los numeros M (" + ejemploM + ") y N (" + ejemploN + ") da como resultado: " + resultEj14);
+        System.out.println("La serie de Ackermann para los numeros M (" + ejemploM + ") y N (" + ejemploN + ") da como resultado: " + resultEj14+"\n");
 
         /*Testeo ej15*/
         int exampN = 10;
         int exampM = 5;
         int resultMCD = mcd(exampM, exampM);
-        System.out.println("El resultado del mcd de los numeros " + exampM + " y " + exampN + " es " + resultMCD);
+        System.out.println("El resultado del mcd de los numeros " + exampM + " y " + exampN + " es " + resultMCD+"\n");
+
+        /*Testeo ej16*/
+        List<Integer> nums = new ArrayList<>();
+        for(int i = 1; i<=10; i++){
+            nums.add(10*i);
+        }
+        int inicio = 5;
+        System.out.println("Mostrando la lista dada en orden inverso: ");
+        mostrarLista(nums, inicio);
     }
 }
