cc70abc86ff09f7aa153be0ec1e0963fa28c1991
Artcraft <arturoao@uma.es>
2024-09-12 09:34:12 +0200

    Added the solution of exercise 15, refactored Matematicas.java

--- src/main/java/practica0/Matematicas.java
+++ src/main/java/practica0/Matematicas.java
@@ -2,43 +2,67 @@
 package practica0;
 
 public class Matematicas {
-    public static int fibonacci(int n){
-        if(n<0){
+    public static int fibonacci(int n) {
+        if (n < 0) {
             throw new IllegalArgumentException("El numero no puede ser negativo");
-        } else if (n==0) {
+        } else if (n == 0) {
             return 0;
-        } else if (n==1) {
+        } else if (n == 1) {
             return 1;
-        }else {
-            return fibonacci(n-1)+fibonacci(n-2);
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
         }
     }
 
     /*Ejercicio 14 del pdf Practica 0*/
-    public static int ackermann(int m, int n){
-        if(m<0||n<0){
+    public static int ackermann(int m, int n) {
+        if (m < 0 || n < 0) {
             throw new IllegalArgumentException("Ninguno de los dos numeros puede ser negativo");
         } else if (m > 3) {
             throw new IllegalArgumentException("El valor de m es demasiado grande, puede causar un desbordamiento de pila.");
         }
-        if(m==0){
-            return n+1; 
-        } else if (n==0) {
-            return ackermann(m-1, 1);
-        }else {
-            return ackermann(m-1, ackermann(m, n-1));
+        if (m == 0) {
+            return n + 1;
+        } else if (n == 0) {
+            return ackermann(m - 1, 1);
+        } else {
+            return ackermann(m - 1, ackermann(m, n - 1));
         }
     }
-    public static void main(String[] args){
+
+    /*Ejercicio 15 del pdf Practica 0*/
+    public static int mcd(int m, int n) {
+        if (m < 0 || n < 0) {
+            throw new IllegalArgumentException("Ninguno de los dos numeros puede ser negativo");
+        }
+        if (m < n) {
+            int aux = 0;
+            aux = n;
+            n = m;
+            m = aux;
+        }
+        if (n == 0) {
+            return m;
+        }
+        return mcd(n, m % n);
+    }
+
+    public static void main(String[] args) {
         /*Testeo para ej13*/
         int ejemplo = 20;
         int resultEjemplo = fibonacci(ejemplo);
-        System.out.println("La serie de Fibonacci del numero "+ejemplo+" es "+resultEjemplo);
+        System.out.println("La serie de Fibonacci del numero " + ejemplo + " es " + resultEjemplo);
 
         /*Testeo ej14*/
         int ejemploM = 3;
         int ejemploN = 3;
-        int resultEj14=ackermann(ejemploM, ejemploN);
-        System.out.println("La serie de Ackermann para los numeros M ("+ejemploM+") y N ("+ejemploN+") da como resultado: "+resultEj14);
+        int resultEj14 = ackermann(ejemploM, ejemploN);
+        System.out.println("La serie de Ackermann para los numeros M (" + ejemploM + ") y N (" + ejemploN + ") da como resultado: " + resultEj14);
+
+        /*Testeo ej15*/
+        int exampN = 10;
+        int exampM = 5;
+        int resultMCD = mcd(exampM, exampM);
+        System.out.println("El resultado del mcd de los numeros " + exampM + " y " + exampN + " es " + resultMCD);
     }
 }
